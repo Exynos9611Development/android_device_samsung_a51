7ff2e1ac6153402603729a63b2150cda0aaa63ba
Cat658011 <cat658011@gmail.com>
2024-06-07 13:31:37 +0500

    a51: Decommonize Udfps Handler See history https://github.com/Exynos9611Development/android_device_samsung_universal9611-common/commits/d191be80215d131e784c3321f7a0026c53e9630b/udfps

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 # Fingerprint 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.samsung \
-    sensors.samsung
+    sensors.samsung \
+    UdfpsHandler
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
