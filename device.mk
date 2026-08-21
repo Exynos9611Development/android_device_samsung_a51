591725b14cb6a91fa039e302bd4dec2d2e4702f1
cat658011 <cat658011@gmail.com>
2026-08-21 23:56:05 +0500

    Revert "a51: Revert "a51: Merge mixer_paths and mixer_gains into one file""
    This reverts commit f8c0e45d16971733756cb6c9c981995a1ede34d3.

--- device.mk
+++ device.mk
@@ -44,8 +44,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio configs
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
-    $(DEVICE_PATH)/configs/audio/mixer_gains_ti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains_ti.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths_ti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ti.xml \
     $(DEVICE_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
